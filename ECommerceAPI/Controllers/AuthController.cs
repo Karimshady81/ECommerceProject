8743aa9b5dc5c8669ac7aebe3f908f620c4e6b4c
Karim shady <kshady960@gmail.com>
2025-12-13 19:16:33 +0200

    Fix minor bugs

--- ECommerceAPI/Controllers/AuthController.cs
+++ ECommerceAPI/Controllers/AuthController.cs
@@ -1,22 +1,37 @@
 ﻿using Microsoft.AspNetCore.Http;
 using ECommerceAPI.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using ECommerceAPI.Application.DTOs.Request;
 
 namespace ECommerceAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/users")]
     [ApiController]
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
-        private readonly IJwtService _jwtService;
 
-        public AuthController(IAuthService authService, IJwtService jwtService)
+        public AuthController(IAuthService authService)
         {
             _authService = authService;
-            _jwtService = jwtService;
         }
 
-
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser(RegisterUserRequestDto user)
+        {
+            try
+            {
+                var createdUser = await _authService.RegisterUserAsync(user);
+                return Ok(createdUser);
+            }
+            catch (InvalidDataException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Internal server error: " + ex.Message });
+            }
+        }
     }
 }
