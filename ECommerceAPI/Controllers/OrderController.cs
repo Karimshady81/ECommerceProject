5e87618c52047873eb054df4f13c83b217bd1a9f
Karim shady <kshady960@gmail.com>
2025-12-15 16:40:18 +0200

    Implement 'GetOrder' orderController

--- ECommerceAPI/Controllers/OrderController.cs
+++ ECommerceAPI/Controllers/OrderController.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerceAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/order")]
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("get_order/{orderId}")]
+        public async Task<IActionResult> GetOrderWithDetails (int orderId)
+        {
+            try
+            {
+                var order = await _orderService.GetOrderWithDetailsAsync(orderId);
+                return Ok(order);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    message = ex.Message,
+                    inner = ex.InnerException?.Message
+                });
+            }
+        }
     }
 }
