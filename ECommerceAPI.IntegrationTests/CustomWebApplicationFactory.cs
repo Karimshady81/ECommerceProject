0754e25566df051108546b1d0c4fd258f674718e
Karim shady <kshady960@gmail.com>
2025-12-18 18:22:30 +0200

    Setup the integrationTest layout

---
+++ ECommerceAPI.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace ECommerceAPI.IntegrationTests
+{
+    //This runs my app in memory, not on localhost.
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+    }
+}
