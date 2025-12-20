e1710d18ba022aec315914beb03ab1bb6ac42245
Karim shady <kshady960@gmail.com>
2025-12-20 17:05:09 +0200

    Implement more tests for CartController

--- ECommerceAPI.IntegrationTests/CartControllerTest.cs
+++ ECommerceAPI.IntegrationTests/CartControllerTest.cs
@@ -43,5 +43,28 @@ namespace ECommerceAPI.IntegrationTests
             //Assert
             response.StatusCode.Should().Be(HttpStatusCode.OK);
         }
+
+        [Fact]
+        public async Task AddToCart_WithNoProduct_ReturnBadRequest()
+        {
+            //Arrange
+            var request = new AddToCartRequestDto
+            {
+                UserId = 1,
+                ProductId = 20,
+                Quantity = 2
+            };
+
+            var content = new StringContent(
+                JsonSerializer.Serialize(request),
+                Encoding.UTF8,
+                "application/json");
+
+            //Act
+            var response = await _client.PostAsync($"api/cart/add_to_cart", content);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
     }
 }
